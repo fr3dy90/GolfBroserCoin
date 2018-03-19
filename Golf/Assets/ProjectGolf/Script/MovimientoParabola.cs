73e1579e14413661d232fb9808d55efca5308fc9
Fredy <jfr3dy90@gmail.com>
2018-03-19 17:25:29 -0500

    Inicio fisicas

---
+++ Golf/Assets/ProjectGolf/Script/MovimientoParabola.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoParabola : MonoBehaviour {
+
+    public bool isCalculate;
+    public float disX;
+    public float angle;
+    public TargetBH m_target;
+
+    private void Start()
+    {
+        m_target = FindObjectOfType<TargetBH>();
+    }
+
+    private void Update()
+    {
+        if (isCalculate)
+        {
+            Calculate();
+        }
+    }
+
+    void Calculate()
+    {
+        float radian = angle * Mathf.PI / 180f;
+        if (m_target.target != null)
+        {
+            disX = m_target.target.transform.position.x - transform.position.x;
+        }
+    }
+}
--- Golf/Assets/ProjectGolf/Script/TargetBH.cs
+++ Golf/Assets/ProjectGolf/Script/TargetBH.cs
@@ -6,26 +6,32 @@ public class TargetBH : MonoBehaviour {
 
     public GameObject prefbTarget;
     public GameObject target;
-    public Vector3 offset;
-    public Vector3 targetPos;
+    public Vector3 offset = Vector3.up * 0.01f;
+    
+    public float distance = 1000;
     // Update is called once per frame
 
     private void Start()
     {
         target = Instantiate(prefbTarget, Vector3.zero, Quaternion.Euler(90,0,0));
         target.SetActive(false);
+
+        
+
+        float t = (2 + 22) * 2 / 9.81f;
+        t = Mathf.Sqrt(t);
+        print(t);
     }
 
     void Update () {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray,out hit, 100))
+        if(Physics.Raycast(ray,out hit, distance))
         {
             if(hit.transform.tag == "Ground")
             {
                 target.SetActive(true);
                 target.transform.position = hit.point + offset;
-                targetPos = target.transform.position;
             }
             else
             {
