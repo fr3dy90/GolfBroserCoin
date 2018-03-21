f6bb2ab3ce3579ecae52931f9c8cd52398aa0b37
fr3dy <jfr3dy90@gmail.com>
2018-03-20 20:54:01 -0500

    Script Orden interno

--- Golf/Assets/ProjectGolf/Script/MovimientoParabola.cs
+++ Golf/Assets/ProjectGolf/Script/MovimientoParabola.cs
@@ -11,8 +11,14 @@ public class MovimientoParabola : MonoBehaviour {
     
     public Transform tr;
     bool canShoot;
+
+    int res =30;
+    Vector3[] points;
+    public LineRenderer lr;
+
     private void Start()
     {
+        points = new Vector3[res];
         canShoot = true;
     }
 
@@ -26,23 +32,10 @@ public class MovimientoParabola : MonoBehaviour {
 
     void Calculate()
     {
-        //float radian = angle * Mathf.PI / 180f;
-
-        //float t = (0 + 22 + 2) * 2 / 9.81f;
-        //t = Mathf.Sqrt(t);
-        //print(t);
-
-        //float vec = 22 / (Mathf.Cos(radian) * t);
-        //print(vec);
         disX = tr.position.x - transform.position.x;
         disZ = tr.position.z - transform.position.z;
-        float radian = angle * Mathf.PI / 180f;
-
-        float t = ((-1 * tr.position.y) + transform.position.y + disX) * 2 / 9.81f;
-        t = Mathf.Sqrt(t);
-
-        float vec = disX / (Mathf.Cos(radian) * t);
         
+        RenderPath(new Vector3(vec() * Mathf.Cos(radian()), vec() * Mathf.Sin(radian()), 0));
 
         if (canShoot)
         {
@@ -50,9 +43,45 @@ public class MovimientoParabola : MonoBehaviour {
             {
                 canShoot = false;
                 transform.GetComponent<Rigidbody>().useGravity = true;
-                transform.GetComponent<Rigidbody>().AddForce(vec * Mathf.Cos(radian), vec * Mathf.Sin(radian), 0, ForceMode.Impulse);
+                transform.GetComponent<Rigidbody>().AddForce(vec() * Mathf.Cos(radian()), vec() * Mathf.Sin(radian()), 0, ForceMode.Impulse);
             }
         }
+    }
 
+    public float radian()
+    {
+        float radian = angle * Mathf.PI / 180f;
+        return radian;
+    }
+
+    public float vec()
+    {
+        float vec = disX / (Mathf.Cos(radian()) * timeSimulate());
+        return vec;
+    }
+
+    public float timeSimulate()
+    {
+        float t = ((-1 * tr.position.y) + transform.position.y + disX) * 2 / 9.81f;
+        t = Mathf.Sqrt(t);
+        return t;
+    }
+
+    public void RenderPath(Vector3 path)
+    {
+        Vector3 refG = new Vector3(0f,-9.81f, 0f);
+
+        for (int i = 0; i < res; i++)
+        {
+            float t = i / (float)res * timeSimulate();
+            Vector3 desp = path * t + refG * t * t / 2f;
+            desp += transform.position;
+            points[i] = desp;
+            if (i > 0)
+            {
+                Debug.DrawLine(points[i], points[i - 1], Color.green);
+            }
+            lr.SetPositions(points);
+        }
     }
 }
